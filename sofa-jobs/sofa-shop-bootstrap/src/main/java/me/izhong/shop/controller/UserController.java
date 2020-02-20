1da5c54b8811913c68dd2021746895ef3190ca03
lei Guo <leiguo@leideMacBook-Pro.local>
2020-02-20 23:57:50 +0800

    add redis

--- sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/controller/UserController.java
+++ sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/controller/UserController.java
@@ -1,7 +1,10 @@
 package me.izhong.shop.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import me.izhong.common.annotation.AjaxWrapper;
 import me.izhong.shop.annotation.RequireUserLogin;
 import me.izhong.shop.config.JWTProperties;
@@ -11,18 +14,18 @@ import me.izhong.shop.service.IUserService;
 import me.izhong.shop.service.impl.ThirdPartyService;
 import me.izhong.shop.util.JWTUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Api
 @RestController
+@AjaxWrapper
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired private IUserService userService;
@@ -50,31 +53,33 @@ public class UserController {
 //        return "Success.";
 //    }
 //
-//    @PostMapping("/certify")
-//    @ApiOperation(value="用户实名认证",httpMethod = "POST")
-//    @ResponseBody
+    @PostMapping("/certify")
+    @ApiOperation(value="用户实名认证",httpMethod = "POST")
+    @ResponseBody
 //    @RequireUserLogin
-//    public String certify(HttpServletRequest request) {
-//        String userId = (String)request.getAttribute("userId");
-//        if (StringUtils.isEmpty(userId)) {
-//            throw new RuntimeException("没有找到User Id");
-//        }
-//        User user = userService.findById(Long.valueOf(userId));
-//        userService.certify(user);
-//        return "Success.";
-//    }
+    public String certify(HttpServletRequest request) {
+        String userId = (String)request.getAttribute("userId");
+        if (StringUtils.isEmpty(userId)) {
+            throw new RuntimeException("没有找到User Id");
+        }
+        User user = userService.findById(Long.valueOf(userId));
+        userService.certify(user);
+        return "Success.";
+    }
 //
-//    @PostMapping("/register/phoneCode")
-//    @ApiOperation(value="获取验证码",httpMethod = "POST")
-//    @ResponseBody
-//    public String getPhoneCode(User user) {
-//        // TODO a valid phone number and valid attempt to send sms
-//        String res = thirdService.sendSms(user.getPhone());
-//        if (res != null) {
-//            return res;
-//        }
-//        return "Success.";
-//    }
+    @PostMapping("/register/phoneCode")
+    @ApiOperation(value="获取验证码",httpMethod = "POST")
+    @ResponseBody
+    public String getPhoneCode(@RequestParam("phone")String phoneNumber) {
+        // TODO a valid phone number and valid attempt to send sms
+        String randomNumber = RandomStringUtils.randomNumeric(6);
+        String res = thirdService.sendSms(phoneNumber, new JSONObject(){{put("code", randomNumber);}});
+        if (res != null) {
+            log.info("sms res:" + res);
+            return res;
+        }
+        return "Success.";
+    }
 
 //    private void validateInput(User user) {
 //        userService.expectNew(user);
--- sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/service/impl/ThirdPartyService.java
+++ sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/service/impl/ThirdPartyService.java
@@ -1,19 +1,30 @@
 package me.izhong.shop.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.nacos.client.utils.JSONUtils;
+import lombok.extern.slf4j.Slf4j;
 import me.izhong.shop.config.AliCloudProperties;
 import me.izhong.shop.response.ali.CertifyServiceResponse;
 import me.izhong.shop.response.ali.SmsResponse;
 import me.izhong.shop.util.AliCloudUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Component
+@Slf4j
 public class ThirdPartyService {
     private AliCloudProperties properties;
+    private StringRedisTemplate redisTemplate;
 
     @Autowired
-    public ThirdPartyService(AliCloudProperties properties){
+    public ThirdPartyService(AliCloudProperties properties, StringRedisTemplate redisTemplate) {
         this.properties = properties;
+        this.redisTemplate = redisTemplate;
     }
 
     public String getCertifiedInfo(String personName, String idCard) {
@@ -24,11 +35,13 @@ public class ThirdPartyService {
         return null;
     }
 
-    public String sendSms(String phoneNumber) {
-        SmsResponse response = AliCloudUtils.instance.sendSms(properties, phoneNumber);
+    public String sendSms(String phoneNumber, JSONObject params) {
+        String paramString = params.toString();
+        SmsResponse response = AliCloudUtils.instance.sendSms(properties, phoneNumber, paramString);
         if (!response.isSuccess()) {
             return response.getCode() + "," + response.getMessage();
         }
+        redisTemplate.opsForValue().set(phoneNumber, params.getString("code"));
         return null;
     }
 }
