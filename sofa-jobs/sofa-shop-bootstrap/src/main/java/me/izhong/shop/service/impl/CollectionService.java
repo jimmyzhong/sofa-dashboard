31df0127ab7f03e2d14abf2f100aad27898baad5
qhliu <l_qh_vip@163.com>
2020-03-05 00:48:53 +0800

    我的收藏demo

--- sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/service/impl/CollectionService.java
+++ sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/service/impl/CollectionService.java
@@ -1,6 +1,8 @@
 package me.izhong.shop.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
@@ -11,7 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.google.common.collect.Lists;
 
 import me.izhong.shop.dao.CollectionDao;
+import me.izhong.shop.dao.GoodsDao;
 import me.izhong.shop.dto.GoodsCollectionParam;
+import me.izhong.shop.entity.Goods;
 import me.izhong.shop.entity.UserCollection;
 import me.izhong.shop.service.ICollectionService;
 
@@ -20,15 +24,20 @@ public class CollectionService implements ICollectionService {
 
 	@Autowired
 	private CollectionDao collectionDao;
+	@Autowired
+	private GoodsDao goodsDao;
 
 	@Override
 	@Transactional
-	public void add(GoodsCollectionParam param) {
-		UserCollection collection = collectionDao.findByUserIdAndProductId(param.getUserId(), param.getProductId());
+	public void add(Long userId, Long productId) {
+		UserCollection collection = collectionDao.findByUserIdAndProductId(userId, productId);
 		if (collection == null) {
-			UserCollection newCollection = new UserCollection();
-			BeanUtils.copyProperties(param, newCollection);
-			collectionDao.save(newCollection);
+			collection = new UserCollection();
+			collection.setUserId(userId);
+			collection.setProductId(productId);
+			collection.setCreateTime(LocalDateTime.now());
+			collection.setUpdateTime(LocalDateTime.now());
+			collectionDao.save(collection);
 		}
 	}
 
@@ -42,9 +51,16 @@ public class CollectionService implements ICollectionService {
 	public List<GoodsCollectionParam> list(Long userId) {
 		List<UserCollection> items = collectionDao.findByUserId(userId);
 		if (items != null) {
-			return items.stream().map(item -> {
+			return items.stream().map(t -> {
 				GoodsCollectionParam param = new GoodsCollectionParam();
-				BeanUtils.copyProperties(item, param);
+				BeanUtils.copyProperties(t, param);
+				Optional<Goods> goodsOptional = goodsDao.findById(t.getProductId());
+				if (goodsOptional.isPresent()) {
+					Goods goods = goodsOptional.get();
+					param.setProductName(goods.getProductName());
+					param.setProductPic(goods.getProductPic());
+					param.setProductPrice(goods.getPromotionPrice().toString());
+				}
 				return param;
 			}).collect(Collectors.toList());
 		}
