5a992f61c15b3687d210d2a5f6453b51c4d552ea
qhliu <l_qh_vip@163.com>
2020-03-06 19:07:15 +0800

    update cartItem model

--- sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/entity/CartItem.java
+++ sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/entity/CartItem.java
@@ -1,6 +1,5 @@
 package me.izhong.shop.entity;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
@@ -27,26 +26,10 @@ public class CartItem {
     private Long userId;
     @Column(name = "PRODUCT_ID")
     private Long productId;
-    @Column(name = "PRODUCT__ATTR_ID")
+    @Column(name = "PRODUCT_ATTR_ID")
     private Long productAttributeId; // 购物车存放的也可能是具体某个商品规格
     @Column(name = "QUANTITY")
     private Integer quantity;
-    @Column(name = "PRICE")
-    private BigDecimal price;
-    @Column(name = "RSV1")
-    private String rsv1;
-    @Column(name = "RSV2")
-    private String rsv2;
-    @Column(name = "RSV3")
-    private String rsv3;
-    @Column(name = "PRODUCT_PIC")
-    private String productPic;
-    @Column(name = "PRODUCT_NAME")
-    private String productName;
-    @Column(name = "PRODUCT_SN")
-    private String productSn;
-    @Column(name = "NICK_NAME")
-    private String nickName;
     @Column(name = "CHECKED")
     private Integer checked;
     @Column(name = "IS_DELETE")
