f28c80802dfc82bf898a298231bb4c74b48f156e
lei Guo <leiguo@leideMacBook-Pro.local>
2020-03-15 20:46:01 +0800

    part

--- sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/entity/PayRecord_.java
+++ sofa-jobs/sofa-shop-bootstrap/src/main/java/me/izhong/shop/entity/PayRecord_.java
@@ -3,9 +3,10 @@ package me.izhong.shop.entity;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @StaticMetamodel(PayRecord.class)
-public class PayRecord_ {
+public class PayRecord_ extends PersistedEntity{
     public static volatile SingularAttribute<PayRecord, Long> id;
     public static volatile SingularAttribute<PayRecord, BigDecimal> totalAmount;
     public static volatile SingularAttribute<PayRecord, BigDecimal> payAmount;
@@ -17,4 +18,5 @@ public class PayRecord_ {
     public static volatile SingularAttribute<PayRecord, String> comment;
     public static volatile SingularAttribute<PayRecord, Long> payerId;
     public static volatile SingularAttribute<PayRecord, Long> receiverId;
+    public static volatile SingularAttribute<PayRecord, LocalDateTime> createTime;
 }
