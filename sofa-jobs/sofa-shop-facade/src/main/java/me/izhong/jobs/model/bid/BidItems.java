df2f620811e98f7809c3b0173ae679cf0c289f9e
jimmy <jjzhong_mac@izhongme>
2020-03-27 22:17:13 +0800

    fix

--- sofa-jobs/sofa-shop-facade/src/main/java/me/izhong/jobs/model/bid/BidItems.java
+++ sofa-jobs/sofa-shop-facade/src/main/java/me/izhong/jobs/model/bid/BidItems.java
@@ -5,16 +5,32 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class BidItems implements Serializable {
 
-    private Long allow;
 
+    /**
+     * 出价序号
+     */
     private Long seqId;
 
+    /**
+     * 出价用户ID
+     */
+    private Long userId;
+
+    /**
+     * 出价价格，单位分
+     */
     private Long price;
+
+    /**
+     * 出价时间
+     */
+    private Date bidTime;
 
 }
