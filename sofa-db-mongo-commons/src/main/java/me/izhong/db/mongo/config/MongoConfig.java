3e85c617ad66ba25da762ac2933b8af6f1b632ce
jimmy <jjzhong_mac@izhongme>
2020-04-15 11:30:16 +0800

    去掉默认mapper添加的_class

--- sofa-db-mongo-commons/src/main/java/me/izhong/db/mongo/config/MongoConfig.java
+++ sofa-db-mongo-commons/src/main/java/me/izhong/db/mongo/config/MongoConfig.java
@@ -1,11 +1,14 @@
 package me.izhong.db.mongo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.convert.CustomConversions;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
@@ -19,12 +22,17 @@ public class MongoConfig  {
     }
 
     @Bean
-    public MongoTemplate mongoTemplate(MongoDbFactory factory) throws Exception {
-        //remove _class
-        MappingMongoConverter converter = new MappingMongoConverter(factory, new MongoMappingContext());
-        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
-        MongoTemplate mongoTemplate = new MongoTemplate(factory, converter);
+    public MongoTemplate mongoTemplate(MongoDbFactory factory,MappingMongoConverter mappingMongoConverter) {
+        MongoTemplate mongoTemplate = new MongoTemplate(factory, mappingMongoConverter);
         return mongoTemplate;
-
+    }
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext context, BeanFactory beanFactory, CustomConversions conversions) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, context);
+        mappingConverter.setCustomConversions(beanFactory.getBean(CustomConversions.class));
+        mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));//去掉默认mapper添加的_class
+        //mappingConverter.setCustomConversions(conversions);//添加自定义的转换器
+        return mappingConverter;
     }
 }
